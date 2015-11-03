54952512deb1a389d61ee1fd6ac659afd3c6d9b6
Patricia Decker <patriciasquared@gmail.com>
2015-11-02 21:06:54 -0800

    updating database classes, still in yelp

--- model.py
+++ model.py
@@ -20,30 +20,61 @@ db = SQLAlchemy()
 
 class YelpBiz(db.Model):
     """Business in Yelp Academic Dataset."""
-    __tablename__ = "yelpBusinesses"
+    __tablename__ = "yelpBiz"
 
-    pass
+    #TODO: verify db.datatypes
+    business_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    address = db.Column(db.String(200), nullable=False)
+    city = db.Column(db.String(64), nullable=False)
+    state = db.Column(db.String(20), nullable=False)
+    lat = db.Column(db.Integer, nullable=False) #TODO: is there a lat/long type?
+    lon = db.Column(db.Integer, nullable=False)
+    stars = db.Column(db.Integer, nullable=True)
+    review_count = db.Column(db.Integer, nullable=True)
+    photo_url = db.Column(db.String(200), nullable=True)
+    is_open = db.Column(db.Boolean, nullable=False)
+    url = db.Column(db.String(200), nullable=False)
+    # TODO: neighborhoods
+    # TODO: schools (nearby universities)
+
+    def __repr__(self):
+        return "<YelpBiz business_id=%d name=%s>" % (self.business_id, self.name)
 
 
 class YelpUser(db.Model):
     """User in Yelp Academic Dataset."""
     __tablename__ = "yelpUsers"
 
-    pass
+    user_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=False)
+    review_count = db.Column(db.Integer, nullable=True)  # a user might have no reviews
+    average_stars = db.Column(db.Float, nullable=True)  # this is calculable from other tables
+    # TODO: votes
+
+    def __repr__(self):
+        return "<YelpUser user_id=%d name=%s>" % (self.user_id, self.name)
 
 
 class YelpReview(db.Model):
     """Review in Yelp Academic Dataset."""
     __tablename__ = "yelpReviews"
 
-    pass
+    business_id = db.Column(db.Integer, db.ForeignKey('yelpBiz.business_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('yelpUsers.user_id'))
+    stars = db.Column(db.Integer, nullable=False)  # integer 1-5 #TODO: restrict or check
+    # text = db.Column(db.)
+
+    def __repr__(self):
+        return "<>"
 
 
 class PlatePalBiz(db.Model):
     """Business on PlatePal website. (Builds on Yelp database.)"""
-    __tablename__ = "businesses"
+    __tablename__ = "biz"
 
-    pass
+    def __repr__(self):
+        return "<>"
 
 class PlatePalUser(db.Model):
     """
@@ -52,31 +83,36 @@ class PlatePalUser(db.Model):
     """
     __tablename__ = "users"
 
-    pass
+    def __repr__(self):
+        return "<>"
 
 class PlatePalRating(db.Model):
     """User-generated score of a business for a specific category."""
     __tablename__ = "ratings"
 
-    pass
+    def __repr__(self):
+        return "<>"
 
 class UserList(db.Model):
     """User-generated list by category of restaurants."""
     __tablename__ = "lists"
 
-    pass
+    def __repr__(self):
+        return "<>"
 
 class ListEntry(db.Model):
     """Restaurant entries in user-generated list, UserList."""
     __tablename__ = "entries"
 
-    pass
+    def __repr__(self):
+        return "<>"
 
 class Category(db.Model):
     """Categories for classification and targeting sentiment analysis."""
     __tablename__ = "categories"
 
-    pass
+    def __repr__(self):
+        return "<>"
 
 class Classification(db.Model):
     """
@@ -85,7 +121,8 @@ class Classification(db.Model):
     Classifier trained user subset of restaurant reviews in Yelp dataset."""
     __tablename__ = "classifications"
 
-    pass
+    def __repr__(self):
+        return "<>"
 
 class ReviewClassification(db.Model):
     """
@@ -96,13 +133,16 @@ class ReviewClassification(db.Model):
     """
     __tablename__ = "revclasses"
 
-    pass
+    def __repr__(self):
+        return "<>"
+
 
 class Sentiment(db.Model):
     """Calculation table for aggregate sentiment for a business-category pair."""
     __tablename__ = "sentiments"
 
-    pass
+    def __repr__(self):
+        return "<>"
 
 
 ##############################################################################

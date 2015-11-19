df17971a70b27c13b0ef0b9d6635d178639ac274
Patricia Decker <patriciasquared@gmail.com>
2015-11-18 23:02:26 -0800

    worked through word2vec example; not much additional information

--- pdclassifier.py
+++ pdclassifier.py
@@ -481,7 +481,7 @@ class PennTreebankPunkt(object):
                 for i, word in enumerate(words):
                     words[i] = self.check_contraction(word)
                 # append list of words to list of sentences
-                sentence_list.append(words)             
+                sentence_list.append(words)        
             return sentence_list
         elif self.use_flag == 'sentences':
             return raw_sentence_list
@@ -931,8 +931,12 @@ def plot_sentiment_model_scores(scores_by_nfeats):
 # First, to train Word2Vec it is better not to remove stop words because
 # the algorithm relies on the broader context of the sentence in order
 # to produce high-quality word vectors.
-from gensim.models import word2vec
+from gensim.models import word2vec # for making model
+from gensim.models import Word2Vec # for loading model
 import logging
+from sklearn.cluster import KMeans
+from nltk.corpus import stopwords
+import time
 
 def clustering_study():
     """using distributed word vectors created by the Word2Vec algorithm,
@@ -952,27 +956,55 @@ def clustering_study():
 
     >>> model.most_similar("gluten")
     [(u'restaurants', 0.966518759727478), (u'except', 0.9627533555030823), (u'brunch', 0.9542723298072815), (u'their', 0.952192485332489), (u'seems', 0.9517408013343811), (u'prefer', 0.9516568183898926), (u'Indian', 0.9506375789642334), (u'Now', 0.9481799602508545), (u'ambiance', 0.9477816820144653), (u'high', 0.9476841688156128)]
+
+    >>> model.most_similar("vegan")
+    [(u'burgers', 0.8741724491119385), (u'Their', 0.864501953125), (u'vegetarian', 0.8616198301315308), (u'best', 0.8526089191436768), (u'tastes', 0.8514992594718933), (u'pizza', 0.8467479944229126), (u'etc', 0.8420628309249878), (u'interesting', 0.8413756489753723), (u'filling', 0.8396750688552856), (u'They', 0.8361315727233887)]
+
     """
     container_path = './data/random_forest/'
     print "... Loading data from %s ..." % container_path
 
     documents = loads_yelp_reviews(container_path)
+    y = documents.target
+    for i in range(len(y)):
+        if y[i] in [0, 3, 4, 5]:
+            y[i] = 0
+        else:
+            y[i] = 1
+    y = np.array(y)
+    
+    documents_pd = loads_pdecks_reviews()
+    # transform toy dataset labels to 0 = gluten, 1 = unknown
+    y_pd = documents_pd.target
+    for i in range(len(y_pd)):
+        if y_pd[i] == 1 or target == 2:
+            # good
+            y_pd[i] = 1
+        else:
+            # bad
+            y_pd[i] = 0
+    y_pd = np.array(y_pd)
 
     preprocessor = PennTreebankPunkt('word2vec')
 
     sentences = [] # initialize an empty list of sentences
-    print "Parsing sentences from labeled set"
+    print "Parsing sentences from training set"
     for doc in documents.data:
         sentences += preprocessor(doc)
+
+    test_sentences = [] # initialize an empty list of sentences
+    print "Parsing sentences from test set"
+    for doc in documents_pd.data:
+        test_sentences += preprocessor(doc)
 
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',\
         level=logging.INFO)
 
     # Set values for various parameters
     num_features = 300    # Word vector dimensionality
-    min_word_count = 40   # Minimum word count
+    min_word_count = 10   # Minimum word count
     num_workers = 4       # Number of threads to run in parallel
-    context = 10          # Context window size
+    context = 5          # Context window size
     downsampling = 1e-3   # Downsample setting for frequent words
 
     # Initialize and train the model (this will take some time)
@@ -985,16 +1017,181 @@ def clustering_study():
     # init_sims will make the model much more memory-efficient.
     model.init_sims(replace=True)
 
-    # It can be helpful to create a meaningful model name and
-    # save the model for later use. You can load it later using Word2Vec.load()
-    model_name = "300features_40minwords_10context"
+
+    #PERSIST the model
+    model_name = "300features_10minwords_5context"
     model.save(model_name)
+
+    #REVIVE the model
+    # model = Word2Vec.load("300features_40minwords_10context")
+    model = Word2Vec.load("300features_10minwords_5context")
+
+    # feature vector for each word in vocab, stored in np array syn0
+    # model.syn0.shape >> (737,300) == 737 300-feature-long words, 40 word min
+    # model.syn0.shape >> (2182,300) == 2182 300-feature-long words, 10 word min
+
+    clean_train_reviews = []
+    for review in documents.data:
+        clean_train_reviews.append( review_to_wordlist( review, remove_stopwords=True))
+
+    trainDataVecs = getAvgFeatureVecs( clean_train_reviews, model, num_features )
+
+    clean_test_reviews = []
+    for review in documents_pd.data:
+        clean_test_reviews.append( review_to_wordlist( review, remove_stopwords=True))
+
+    testDataVecs = getAvgFeatureVecs( clean_test_reviews, model, num_features )
+
+
+    # KMeans Clustering
+    start = time.time() # Start time
+
+    # Set "k" (num_clusters) to be 1/5th of the vocabulary size, or an
+    # average of 5 words per cluster
+    word_vectors = model.syn0
+    num_clusters = word_vectors.shape[0] / 5
+
+    # Initalize a k-means object and use it to extract centroids
+    kmeans_clustering = KMeans( n_clusters = num_clusters )
+    idx = kmeans_clustering.fit_predict( word_vectors )
+
+    # Get the end time and print how long the process took
+    end = time.time()
+    elapsed = end - start
+    print "Time taken for K Means clustering: ", elapsed, "seconds."
+    
+    # Create a Word / Index dictionary, mapping each vocabulary word to
+    # a cluster number                                                                                            
+    word_centroid_map = dict(zip( model.index2word, idx ))
+
+    # For the first 10 clusters
+    for cluster in xrange(0,10):
+        #
+        # Print the cluster number  
+        print "\nCluster %d" % cluster
+        #
+        # Find all of the words for that cluster number, and print them out
+        words = []
+        for i in xrange(0,len(word_centroid_map.values())):
+            if( word_centroid_map.values()[i] == cluster ):
+                words.append(word_centroid_map.keys()[i])
+        print words
+
+
+    # now we have a cluster (or "centroid") assignment for each word,
+    # and we can define a function to convert reviews into bags-of-centroids.
+    # This works just like Bag of Words but uses semantically related clusters
+    # instead of individual words:
+
+    # Pre-allocate an array for the training set bags of centroids (for speed)
+    train_centroids = np.zeros( (len(documents.data), num_clusters), \
+        dtype="float32" )
+    
+    test_centroids = np.zeros( (len(documents_pd.data), num_clusters), \
+        dtype="float32" )
+
+    # Transform the training set reviews into bags of centroids
+    counter = 0
+    for review in clean_train_reviews:
+        train_centroids[counter] = create_bag_of_centroids( review, \
+            word_centroid_map )
+        counter += 1
+    
+    counter = 0
+    for review in clean_test_reviews:
+        test_centroids[counter] = create_bag_of_centroids( review, \
+            word_centroid_map )
+        counter += 1
+
+    forest = RandomForestClassifier(n_estimators = 100)
+    forest = forest.fit(train_centroids, y)
+    result = forest.predict(test_centroids)
+
+    # compare the test results 
+    comparison = zip(y_pd, result)
+
+    for item in comparison:
+        print "Target: %d\tResult: %d" % (item[0], item[1])
 
     return
 
+
 # TODO: frequency distributions
+def review_to_wordlist( review, remove_stopwords=False ):
+    """
+    converts a document to a sequence of words optionally
+    removing stop words.  Returns a list of words.
+    """
+    # Remove non-letters
+    review_text = re.sub("[^a-zA-Z]"," ", review)
+    # Convert words to lower case and split them
+    words = review_text.lower().split()
+    # Optionally remove stop words (false by default)
+    if remove_stopwords:
+        stops = set(stopwords.words("english"))
+        words = [w for w in words if not w in stops]
+    return(words)
+
+def create_bag_of_centroids( wordlist, word_centroid_map ):
+    # The number of clusters is equal to the highest cluster index
+    # in the word / centroid map
+    num_centroids = max( word_centroid_map.values() ) + 1
+    # Pre-allocate the bag of centroids vector (for speed)
+    bag_of_centroids = np.zeros( num_centroids, dtype="float32" )
+    # Loop over the words in the review. If the word is in the vocabulary,
+    # find which cluster it belongs to, and increment that cluster count 
+    # by one
+    for word in wordlist:
+        if word in word_centroid_map:
+            index = word_centroid_map[word]
+            bag_of_centroids[index] += 1
+    # Return the "bag of centroids"
+    return bag_of_centroids
 
 
+def makeFeatureVec(words, model, num_features):
+    """average all of the word vectors in a given paragraph"""
+    
+    # Pre-initialize an empty numpy array (for speed)
+    featureVec = np.zeros((num_features,),dtype="float32")
+    
+    nwords = 0.
+     
+    # Index2word is a list that contains the names of the words in 
+    # the model's vocabulary. Convert it to a set, for speed 
+    index2word_set = set(model.index2word)
+    
+    # Loop over each word in the review and, if it is in the model's
+    # vocaublary, add its feature vector to the total
+    for word in words:
+        if word in index2word_set: 
+            nwords = nwords + 1.
+            featureVec = np.add(featureVec,model[word])
+     
+    # Divide the result by the number of words to get the average
+    featureVec = np.divide(featureVec,nwords)
+    return featureVec
+
+
+def getAvgFeatureVecs(reviews, model, num_features):
+    """Given a set of reviews (each one a list of words), calculate 
+    the average feature vector for each one and return a 2D numpy array"""
+    counter = 0.
+     
+    # Preallocate a 2D numpy array, for speed
+    reviewFeatureVecs = np.zeros((len(reviews),num_features),dtype="float32")
+     
+    for review in reviews:
+       # Print a status message every 1000th review
+       if counter%1000. == 0.:
+           print "Review %d of %d" % (counter, len(reviews))
+       # make average feature vectors
+       reviewFeatureVecs[counter] = makeFeatureVec(review, model, \
+           num_features)
+       
+       # Increment the counter
+       counter = counter + 1.
+    return reviewFeatureVecs
 
 
 
